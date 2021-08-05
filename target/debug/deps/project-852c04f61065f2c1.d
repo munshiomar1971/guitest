e208942a36bb6b6dd51a0b54166f509a103200db
worstfrontendcoder <omarsumon1030@gmail.com>
2021-08-04 21:56:39 -0500

    basic gui

---
+++ target/debug/deps/project-852c04f61065f2c1.d
@@ -0,0 +1,5 @@
+/home/omar/Desktop/project/target/debug/deps/project-852c04f61065f2c1: src/main.rs
+
+/home/omar/Desktop/project/target/debug/deps/project-852c04f61065f2c1.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/project.d
@@ -0,0 +1,1 @@
+/home/omar/Desktop/project/target/debug/project: /home/omar/Desktop/project/src/main.rs
